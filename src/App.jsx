942f4640dd6f364dbb4c4f8c26e52d436cf608de
dbrn <dbrn@rocketmail.com>
2020-02-15 19:47:29 +0100

    Added a new react app created via create-react-app and added its scss styles

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+import Homepage from "./Homepage.component.jsx";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Homepage />
+      </div>
+    );
+  }
+}
+
+export default App;
